24c30655e83d95726465bc42a7635560ee351a12
pinjuf <paul.denise9110@gmail.com>
2021-10-23 14:06:48 +0200

    fix div

--- README.md
+++ README.md
@@ -7,4 +7,4 @@
     > You will be asked a passwd, the default on ev3dev is `maker`
  3) CD into `<mountpoint>`
  4) Clone this repo if not already done: `git clone https://github.com/pinjuf/kurzschliesser-ev3dev`
- 5) You can now execute the main.py script using either Brickman or `ssh robot@ev3dev.local ./kurzschliesser-ev3dev/main.py`
+ 5) You can now execute the `main.py` script using either Brickman (recommended) or `ssh robot@ev3dev.local ./kurzschliesser-ev3dev/main.py`
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from ev3dev2.sensor import *
 from ev3dev2.sensor.lego import *
 from ev3dev2.led import *
 from ev3dev2.sound import *
+from ev3dev2.button import *
+from ev3dev2.display import *
 print("done.")
 
 CLAW_RANGE   = 2500
@@ -16,11 +18,12 @@ CLAW_LIFT_RANGE = 90
 claw_lift = LargeMotor(OUTPUT_D)
 claw      = MediumMotor(OUTPUT_A)
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
+buttons = Button()
 
 def force_claw_lift_down():
     claw_lift.on(-25)
     while not claw_lift.is_stalled:
-        pass
+        sleep(0.01)
     claw_lift.reset()
 def set_claw_lift(position):
     claw_lift.on_to_position(
@@ -30,13 +33,18 @@ def set_claw_lift(position):
 def force_claw_closed():
     claw.on(-100)
     while not claw.is_stalled:
-        pass
+        sleep(0.01)
     claw.reset()
 def set_claw(position):
     claw.on_to_position(
             100,
             0 if position.lower()=="closed" else CLAW_RANGE)
 
-
+print("Initializing claw... ", end="")
 force_claw_closed()
 force_claw_lift_down()
+print("done.\nWaiting for start signal... ", end="")
+buttons.wait_for_bump("enter")
+print("received.")
+
+print("Program finished.")
