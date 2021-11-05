5d9f1103ade0ac32f4e543f4baa9ad1116dd3bd8
pinjuf <paul.denise9110@gmail.com>
2021-11-05 20:03:21 +0100

    basic intersection handling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 print("Loading libs... ", end="")
-from time import sleep
+from time import time, sleep
 
 from ev3dev2.motor import *
 from ev3dev2.sensor import *
@@ -11,6 +11,8 @@ from ev3dev2.sound import *
 from ev3dev2.button import *
 from ev3dev2.display import *
 print("done.")
+
+DPS_50 = 82.5 # degrees per second on full rotation with 50% power
 
 CLAW_RANGE   = 2500
 CLAW_LIFT_RANGE = 90
@@ -54,6 +56,25 @@ def set_claw(position):
             100,
             0 if position.lower()=="closed" else CLAW_RANGE)
 
+def handle_intersection():
+    if ColorSensor.COLOR_GREEN in [color_left.color, color_right.color]:
+        sleep(0.05)
+        tank_drive.stop()
+        if color_left.color == ColorSensor.COLOR_GREEN and \
+           color_right.color != ColorSensor.COLOR_GREEN:
+               tank_drive.on_for_seconds(25, 25, 1.25)
+               tank_drive.on_for_seconds(50, -50, 90/DPS_50)
+               tank_drive.on_for_seconds(-25, -25, 0.5)
+        if color_right.color == ColorSensor.COLOR_GREEN and \
+           color_left.color != ColorSensor.COLOR_GREEN:
+               tank_drive.on_for_seconds(25, 25, 1.25)
+               tank_drive.on_for_seconds(-50, 50, 90/DPS_50)
+               tank_drive.on_for_seconds(-25, -25, 0.5)
+        return True
+    return False
+
+
+
 print("Initializing claw... ", end="")
 force_claw_lift_down()
 force_claw_closed()
@@ -64,22 +85,30 @@ print("received.")
 while True:
     if ColorSensor.COLOR_NOCOLOR in [color_left.color, color_right.color]:
         tank_drive.stop()
-    elif ultrasound.distance_centimeters < 7.5:
-        tank_drive.stop()
+    elif ultrasound.distance_centimeters < 8:
+        tank_drive.on_for_seconds(50, -50, 180/DPS_50)
+
+    elif handle_intersection():
+        pass
 
     elif color_left.color == ColorSensor.COLOR_BLACK: # turn left
         tank_drive.stop()
         tank_drive.on(50, -25)
         while color_left.color == ColorSensor.COLOR_BLACK:
             sleep(0.01)
-        sleep(0.30) # time padding, make sure we are beyond the line
-        tank_drive.on(50, 50)
+        start = time.time()
+        while time.time()-start <= 0.3: # time padding with intersection
+            if handle_intersection():
+                break
     elif color_right.color == ColorSensor.COLOR_BLACK: # turn right
         tank_drive.stop()
         tank_drive.on(-25, 50)
         while color_right.color == ColorSensor.COLOR_BLACK:
             sleep(0.01)
-        sleep(0.30)
+        start = time.time()
+        while time.time()-start <= 0.3:
+            if handle_intersection():
+                break
         tank_drive.on(50, 50)
 
     else:
