ca233933d7aaaedb7a79621cd49552bfa7132b4e
kurzschliesser <kurzschliesser@protonmail.com>
2021-11-07 18:19:18 +0100

    Fix and improve intersection handling

--- main.py
+++ main.py
@@ -57,12 +57,19 @@ def set_claw(position):
             0 if position.lower()=="closed" else CLAW_RANGE)
 
 def handle_intersection():
-    if color_left.color == ColorSensor.COLOR_BLACK and color_right == ColorSensor.COLOR_BLACK:
+    global check_for_black
+    if color_left.color == color_right.color == ColorSensor.COLOR_BLACK == check_for_black: # we hit a black line at 90 degs
         tank_drive.stop()
         tank_drive.on_for_seconds(25, 25, 0.75)
+        tank_drive.on_for_seconds(50, -50, 0.2) # check if there is black
+        if color_right.color != ColorSensor.COLOR_BLACK:
+            tank_drive.on_for_seconds(-50, 50, 0.2)
+            tank_drive.on_for_seconds(-25, -25, 0.75)
+            check_for_black = 0
+            return False
         return True
-    elif ColorSensor.COLOR_GREEN in [color_left.color, color_right.color]:
-        sleep(0.15) # make sure we are totally on the markers
+    if ColorSensor.COLOR_GREEN in [color_left.color, color_right.color]:
+        sleep(0.1) # make sure we are totally on the markers
         tank_drive.stop()
         if color_left.color == ColorSensor.COLOR_GREEN and \
            color_right.color == ColorSensor.COLOR_GREEN:
@@ -70,11 +77,11 @@ def handle_intersection():
         elif color_left.color == ColorSensor.COLOR_GREEN:
                tank_drive.on_for_seconds(25, 25, 1.25)
                tank_drive.on_for_seconds(50, -50, 90/DPS_50)
-               tank_drive.on_for_seconds(-25, -25, 0.5)
+               tank_drive.on_for_seconds(-25, -25, 0.4)
         elif color_right.color == ColorSensor.COLOR_GREEN:
                tank_drive.on_for_seconds(25, 25, 1.25)
                tank_drive.on_for_seconds(-50, 50, 90/DPS_50)
-               tank_drive.on_for_seconds(-25, -25, 0.5)
+               tank_drive.on_for_seconds(-25, -25, 0.4)
         return True
     return False
 
@@ -82,40 +89,45 @@ def handle_intersection():
 
 print("Initializing claw... ", end="")
 force_claw_lift_down()
+set_claw_lift("up")
 force_claw_closed()
+set_claw("open")
 print("done.\nWaiting for start signal... ", end="")
 buttons.wait_for_bump("enter")
 print("received.")
 
 while True:
+    check_for_black = 1
     if ColorSensor.COLOR_NOCOLOR in [color_left.color, color_right.color]:
         tank_drive.stop()
-    elif ultrasound.distance_centimeters < 8:
+    elif ultrasound.distance_centimeters < 9:
         tank_drive.on_for_seconds(50, -50, 180/DPS_50)
 
     elif handle_intersection():
-        pass
+        continue
 
     elif color_left.color == ColorSensor.COLOR_BLACK: # turn left
         tank_drive.stop()
-        tank_drive.on(50, -25)
         while color_left.color == ColorSensor.COLOR_BLACK:
-            if handle_intersection():
+            if handle_intersection(): # check for intersection
                 break
+            tank_drive.on(50, -25)
         start = time.time()
-        while time.time()-start <= 0.3: # time padding with intersection
+        while time.time()-start <= 0.3: # time padding
             if handle_intersection():
                 break
+            tank_drive.on(50, -25)
     elif color_right.color == ColorSensor.COLOR_BLACK: # turn right
         tank_drive.stop()
-        tank_drive.on(-25, 50)
         while color_right.color == ColorSensor.COLOR_BLACK:
             if handle_intersection():
                 break
+            tank_drive.on(-25, 50)
         start = time.time()
         while time.time()-start <= 0.3:
             if handle_intersection():
                 break
+            tank_drive.on(-25, 50)
         tank_drive.on(50, 50)
 
     else:
