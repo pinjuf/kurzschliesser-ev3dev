554006eef64f4b02e9b110a4ebe7d5b988a0b934
kurzschliesser <kurzschliesser@protonmail.com>
2021-11-11 19:43:14 +0100

    cleanup java-style code

--- balls.py
+++ balls.py
@@ -17,17 +17,15 @@ color_ball = ColorSensor(INPUT_1)
 color_ball.mode = ColorSensor.MODE_COL_REFLECT
 
 def check_for_ball:
-    return (color_ball.reflected_light_intensity > 50)
-
+    return color_ball.reflected_light_intensity > 50
 
 def search:
     tank_drive.on(50, 50)
     while ball_found == false:
-        if ultrasound.distance_centimeters < 9:  #TURN RIGHT TWICE
+        if ultrasound.distance_centimeters < 9:  # TURN RIGHT TWICE
             tank_drive.off()
             tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
             tank_drive.on_for_rotations(-25, -25, 5 * TIRE_CONST)
             tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
         if check_for_ball():
             break
-    return True
