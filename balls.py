9e21a9736cf056fa7559652686b7cf168b34ed5a
Miezekatze64 <74461330+Miezekatze64@users.noreply.github.com>
2021-11-12 10:10:59 +0100

    Fixed function declaration

--- balls.py
+++ balls.py
@@ -16,10 +16,10 @@ ultrasound.mode = UltrasonicSensor.MODE_US_DIST_CM
 color_ball = ColorSensor(INPUT_1)
 color_ball.mode = ColorSensor.MODE_COL_REFLECT
 
-def check_for_ball:
+def check_for_ball():
     return color_ball.reflected_light_intensity > 50
 
-def search:
+def search():
     tank_drive.on(50, 50)
     while ball_found == false:
         if ultrasound.distance_centimeters < 9:  # TURN RIGHT TWICE
