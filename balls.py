459e29de38b75303103ba768230e3b595b0ae4dd
Miezekatze64 <74461330+Miezekatze64@users.noreply.github.com>
2021-11-12 17:00:36 +0100

    Added REFLECTION_LIMIT constant

--- balls.py
+++ balls.py
@@ -10,6 +10,8 @@ from ev3dev2.sound import *
 from ev3dev2.button import *
 from ev3dev2.display import *
 
+REFLECTION_LIMIT = 50   #Limit of dead and alive balls
+
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 
 ultrasound = UltrasonicSensor(INPUT_4)
@@ -19,7 +21,7 @@ color_ball = ColorSensor(INPUT_1)
 color_ball.mode = ColorSensor.MODE_COL_REFLECT
 
 def check_for_ball():
-    return color_ball.reflected_light_intensity > 50
+    return color_ball.reflected_light_intensity > REFLECTION_LIMIT
 
 def search():
     tank_drive.on(50, 50)
