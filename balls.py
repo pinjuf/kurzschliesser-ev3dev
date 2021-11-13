00c8ba674faa5cd8ed4aa6ce140ad23b63028cbd
Miezekatze64 <74461330+Miezekatze64@users.noreply.github.com>
2021-11-13 08:52:22 +0100

    Implemented grab_ball

--- balls.py
+++ balls.py
@@ -9,9 +9,10 @@ from ev3dev2.led import *
 from ev3dev2.sound import *
 from ev3dev2.button import *
 from ev3dev2.display import *
+from main import set_claw, set_claw_lift
 
-REFLECTION_LIMIT = 50   #Limit of dead and alive balls
-ULATRASOUND_DISTANCE = 2 #Distance to wall/ball in cm to see ball using light sensor
+REFLECTION_LIMIT = 50       #Limit of dead and alive balls
+ULATRASOUND_DISTANCE = 2    #Distance to wall/ball in cm to see ball using light sensor
 
 tank_drive = MoveTank(OUTPUT_B, OUTPUT_C)
 claw_lift = LargeMotor(OUTPUT_D)
@@ -30,15 +31,19 @@ def grab_ball():
     #drive back and turn 180 degrees
     tank_drive.on_for_rotations(-50, -50, 5 * TIRE_CONST)
     tank_drive.on_for_seconds(50, -50, 180/(DPS * 50))
-    tank_drive.on_for_rotations(-50, -50, 5 * TIRE_CONST)
 
-    #TODO: implement ball grabbing with claws
+    set_claw_lift("down")       #open claws
+    set_claw("open")
+    tank_drive.on_for_rotations(-50, -50, 5 * TIRE_CONST)   #grab ball
+    set_claw("closed")          #close claws
+    set_claw_lift("up")
+
     return
 
 def search():
     tank_drive.on(50, 50)
     while ball_found == false:
-        if ultrasound.distance_centimeters < ULATRASOUND_DISTANCE:  # Check for ball/wall
+        if ultrasound.distance_centimeters < ULATRASOUND_DISTANCE:  # check for ball/wall
             if check_for_ball():
                 grab_ball()
             else:   #turn right twice
