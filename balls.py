1ab642897eba72ba66b6c8dad28c90aebb811a6d
Miezekatze64 <74461330+Miezekatze64@users.noreply.github.com>
2021-11-13 08:38:07 +0100

    Fixed ball search - fixes #1

--- balls.py
+++ balls.py
@@ -23,13 +23,20 @@ color_ball.mode = ColorSensor.MODE_COL_REFLECT
 def check_for_ball():
     return color_ball.reflected_light_intensity > REFLECTION_LIMIT
 
+def grab_ball():
+    #TODO: implement ball grabbing with claws
+    return
+
 def search():
     tank_drive.on(50, 50)
     while ball_found == false:
-        if ultrasound.distance_centimeters < 3:  # TURN RIGHT TWICE
-            tank_drive.off()
-            tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
-            tank_drive.on_for_rotations(-25, -25, 5 * TIRE_CONST)
-            tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
-        if check_for_ball():
-            break
+        if ultrasound.distance_centimeters < 3:  # Check for ball/wall
+            if check_for_ball():
+                grab_ball();
+            else:   #turn right twice
+                tank_drive.off()
+                tank_drive.on_for_rotations(-50, -50, 10 * TIRE_CONST)
+                tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
+                tank_drive.on_for_rotations(50, 50, 5 * TIRE_CONST)
+                tank_drive.on_for_seconds(50, -50, 90/(DPS * 50))
+                tank_drive.on(50, 50)
