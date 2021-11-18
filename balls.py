de69156e4bf98ff8f4072320006a6beb54df9066
Miezekatze64 <74461330+Miezekatze64@users.noreply.github.com>
2021-11-18 15:38:36 +0100

    changed move values

--- balls.py
+++ balls.py
@@ -22,9 +22,9 @@ def check_for_ball():
 
 def grab_ball():
     #turn 180 degrees
-    tank_drive.on_for_rotations(50, 50, 5 * TIRE_CONST)   #drive back
+    tank_drive.on_for_rotations(50, 50, 50 * TIRE_CONST)   #drive back
     tank_drive.on_for_seconds(48, -50, 180/(DPS * 50))
-    tank_drive.on_for_rotations(50, 50, 5 * TIRE_CONST)   #drive back to ball
+    tank_drive.on_for_rotations(50, 50, 45 * TIRE_CONST)   #drive back to ball
 
     set_claw("open")       #open claws
     set_claw_lift("down")
