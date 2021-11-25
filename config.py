74fdbdbc98b6df10c3d907e15e79e8f9e6e8fcc0
kurzschliesser <kurzschliesser-rbc@protonmail.com>
2021-11-25 14:41:48 +0100

    ...

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-DPS = 2.0 # degrees per second on 1% power
+DPS = 1.9 # degrees per second on 1% power
 TIRE_RAD = 17.5 # mm
 TIME_CONST = 1 # TODO TEMPORARY! USED FOR TIMED ROTATION! TO BE REPLACED WITH POSITIONAL INPUT INSTEAD OF TIME
 
